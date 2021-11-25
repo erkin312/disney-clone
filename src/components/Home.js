a38f36fa5709421559f4b70e7304670ac25bfd95
erkin312 <erkin.kk@gmail.com>
2021-11-25 14:55:31 +0600

    Slider added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import ImageSlider from './ImageSlider';
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <ImageSlider />
+    </Container>
+  );
 };
 
 export default Home;
 
-const Container = styled.div``;
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  positon: relative;
+  overflow-x: hidden;
+  &:before {
+    background: url('/images/home-background.png') center center / cover
+      no-repeat fixed;
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`;
